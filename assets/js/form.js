b6ee1f0c71533493748636237e04faee6c9a644f
MMeggett <117018012+MMeggett@users.noreply.github.com>
2024-05-01 07:49:19 -0400

    initial commit

---
+++ assets/js/form.js
@@ -0,0 +1,9 @@
+const submitButton = document.querySelector('#submit');
+
+let user = localStorage.getItem('count');
+
+counter.textContent = count;
+
+submitButton.addEventListener('click', function () {
+   
+  });
